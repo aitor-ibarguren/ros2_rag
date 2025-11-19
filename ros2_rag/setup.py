dda9e4270c30b5af9501383781c25100717706c2
109053 <109053@TRI.LAN@WKM0737L.TRI.LAN>
2025-11-19 12:21:48 +0100

    Fixes in setup

--- ros2_rag/setup.py
+++ ros2_rag/setup.py
@@ -8,7 +8,7 @@ package_name = 'ros2_rag'
 setup(
     name=package_name,
     version='0.1.0',
-    packages=[package_name, 'llm_wrappers.flan_t5_wrapper'],
+    packages=[package_name, 'ros2_rag.llm_wrappers.flan_t5_wrapper'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
